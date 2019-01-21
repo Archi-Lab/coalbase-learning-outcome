2b01723005347b3b7d48b53acfc2afc5a8f6baed
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-01-21 23:17:24 +0100

    new init commit

---
+++ src/test/java/de/archilab/coalbase/learningoutcome/CoalbaseLearningOutcomeApplicationTests.java
@@ -0,0 +1,17 @@
+package de.archilab.coalbase.learningoutcome;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoalbaseLearningOutcomeApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+
