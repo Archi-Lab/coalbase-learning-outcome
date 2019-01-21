ee3c6e66924e879c29a1ebea668eca64c32f83d7
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-01-21 09:38:21 +0100

    init commit

---
+++ src/main/java/de/coalbase/coalbase/learningoutcome/CoalbaseLearningOutcomeApplication.java
@@ -0,0 +1,14 @@
+package de.coalbase.coalbase.learningoutcome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoalbaseLearningOutcomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoalbaseLearningOutcomeApplication.class, args);
+	}
+
+}
+
