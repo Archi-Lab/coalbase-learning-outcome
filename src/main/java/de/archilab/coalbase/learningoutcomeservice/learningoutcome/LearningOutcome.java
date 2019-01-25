2399c8ea7de4bb704873ce0670ffd1d402a64e05
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-01-25 14:00:55 +0100

    update learningOutcome, make noargs protected

--- src/main/java/de/archilab/coalbase/learningoutcomeservice/learningoutcome/LearningOutcome.java
+++ src/main/java/de/archilab/coalbase/learningoutcomeservice/learningoutcome/LearningOutcome.java
@@ -5,9 +5,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
+
 import javax.persistence.ElementCollection;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,7 +20,7 @@ import lombok.ToString;
 
 @Entity
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @ToString(callSuper = true)
 public class LearningOutcome {
