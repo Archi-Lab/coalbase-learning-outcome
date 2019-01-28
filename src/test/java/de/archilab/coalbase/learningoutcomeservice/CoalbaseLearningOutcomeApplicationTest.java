868d0b39575731bfcfcda68dba1fcb2d9e73c867
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-01-28 13:31:39 +0100

    add new test cases to CoalbaseLearningOutcomeApplicationTest

--- src/test/java/de/archilab/coalbase/learningoutcomeservice/CoalbaseLearningOutcomeApplicationTest.java
+++ src/test/java/de/archilab/coalbase/learningoutcomeservice/CoalbaseLearningOutcomeApplicationTest.java
@@ -6,8 +6,12 @@ import static junit.framework.Assert.assertTrue;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -86,12 +90,96 @@ public class CoalbaseLearningOutcomeApplicationTest {
   }
 
   @Test
+  public void putLearningOutcome() throws Exception {
+    LearningOutcome learningOutcomeToPut = buildSampleLearningOutcome();
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    String json = objectMapper.writeValueAsString(learningOutcomeToPut);
+
+    mvc.perform(put("/learningOutcomes/" + learningOutcomeToPut.getLearningOutcomeIdentifier())
+        .content(json)
+        .contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(201))
+        .andExpect(
+            jsonPath("$.competence.action", is(learningOutcomeToPut.getCompetence().getAction())))
+        .andExpect(jsonPath("$.competence.taxonomyLevel",
+            is(learningOutcomeToPut.getCompetence().getTaxonomyLevel().name())))
+        .andExpect(
+            jsonPath("$.tools[0].value", is(learningOutcomeToPut.getTools().get(0).getValue())))
+        .andExpect(
+            jsonPath("$.tools[1].value", is(learningOutcomeToPut.getTools().get(1).getValue())))
+        .andExpect(jsonPath("$.purpose.value", is(learningOutcomeToPut.getPurpose().getValue())))
+        .andExpect(jsonPath("$._links.self", notNullValue()));
+
+    List<LearningOutcome> learningOutcomes = (List<LearningOutcome>) learningOutcomeRepository
+        .findAll();
+    assertFalse(learningOutcomes.isEmpty());
+    Optional<LearningOutcome> optionalLearningOutcome = this.learningOutcomeRepository
+        .findById(learningOutcomes.get(0).getLearningOutcomeIdentifier());
+    assertTrue(optionalLearningOutcome.isPresent());
+    LearningOutcome savedLearningOutcome = optionalLearningOutcome.get();
+    assertEquals(savedLearningOutcome.getLearningOutcomeIdentifier(),
+        learningOutcomeToPut.getLearningOutcomeIdentifier());
+    assertEquals(savedLearningOutcome.getCompetence(), learningOutcomeToPut.getCompetence());
+    assertEquals(savedLearningOutcome.getTools().get(0), learningOutcomeToPut.getTools().get(0));
+    assertEquals(savedLearningOutcome.getTools().get(1), learningOutcomeToPut.getTools().get(1));
+    assertEquals(savedLearningOutcome.getPurpose(), learningOutcomeToPut.getPurpose());
+  }
+
+  @Test
+  public void patchLearningOutcome() throws Exception {
+    LearningOutcomeIdentifier identifier = this.createLearningOutcomeToRepo();
+    Optional<LearningOutcome> optionalLearningOutcome = this.learningOutcomeRepository.findById(identifier);
+
+    assertTrue(optionalLearningOutcome.isPresent());
+    LearningOutcome learningOutcome = optionalLearningOutcome.orElse(null);
+    assertNotNull(learningOutcome);
+
+    Competence competence = new Competence("Action", TaxonomyLevel.ANALYSIS);
+    Tool tool = new Tool("Tool");
+    Purpose purpose = new Purpose("Purpose");
+    LearningOutcome learningOutcomeToPatch = new LearningOutcome(identifier, competence,
+        Arrays.asList(tool), purpose);
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    String url = "/learningOutcomes/" + identifier.getId().toString();
+
+    mvc.perform(patch(url).content(objectMapper.writeValueAsString(learningOutcomeToPatch))
+        .contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(200))
+        .andExpect(
+            jsonPath("$.competence.action", is(learningOutcomeToPatch.getCompetence().getAction())))
+        .andExpect(jsonPath("$.competence.taxonomyLevel",
+            is(learningOutcomeToPatch.getCompetence().getTaxonomyLevel().name())))
+        .andExpect(
+            jsonPath("$.tools[0].value", is(learningOutcomeToPatch.getTools().get(0).getValue())))
+        .andExpect(jsonPath("$.purpose.value", is(learningOutcomeToPatch.getPurpose().getValue())))
+        .andExpect(jsonPath("$._links.self", notNullValue()));
+
+  }
+
+  @Test
+  public void deleteLearningOutcome() throws Exception {
+    LearningOutcomeIdentifier identifier = this.createLearningOutcomeToRepo();
+    Optional<LearningOutcome> optionalLearningOutcome = this.learningOutcomeRepository.findById(identifier);
+    assertTrue(optionalLearningOutcome.isPresent());
+    LearningOutcome learningOutcome = optionalLearningOutcome.orElse(null);
+    assertNotNull(learningOutcome);
+
+    String url = "/learningOutcomes/" + identifier.getId().toString();
+    mvc.perform(delete(url)).andExpect(status().isNoContent());
+
+    Optional<LearningOutcome> optionalLearningOutcomeDeleted = this.learningOutcomeRepository.findById(identifier);
+    assertFalse(optionalLearningOutcomeDeleted.isPresent());
+
+  }
+
+  @Test
   public void getLearningOutcomeByUUID() throws Exception {
     LearningOutcomeIdentifier identifier = this.createLearningOutcomeToRepo();
-    Optional<LearningOutcome> optionalLearningOutcome = this.learningOutcomeRepository
-        .findById(identifier);
+    Optional<LearningOutcome> optionalLearningOutcome = this.learningOutcomeRepository.findById(identifier);
+
     assertTrue(optionalLearningOutcome.isPresent());
-    LearningOutcome learningOutcome = optionalLearningOutcome.get();
+    LearningOutcome learningOutcome = optionalLearningOutcome.orElse(null);
+    assertNotNull(learningOutcome);
 
     String url = "/learningOutcomes/" + identifier.getId().toString();
 
@@ -104,6 +192,43 @@ public class CoalbaseLearningOutcomeApplicationTest {
         .andExpect(jsonPath("$.tools[1].value", is(learningOutcome.getTools().get(1).getValue())))
         .andExpect(jsonPath("$.purpose.value", is(learningOutcome.getPurpose().getValue())))
         .andExpect(jsonPath("$._links.self", notNullValue()));
+
+  }
+
+  @Test
+  public void getLearningOutcomes() throws Exception {
+    LearningOutcomeIdentifier identifier0 = this.createLearningOutcomeToRepo();
+    LearningOutcomeIdentifier identifier1 = this.createLearningOutcomeToRepo();
+
+    List<LearningOutcome> learningOutcomes = (List<LearningOutcome>) this.learningOutcomeRepository
+        .findAll();
+
+    String url = "/learningOutcomes/";
+
+    mvc.perform(get(url)).andExpect(status().isOk())
+        .andExpect(content().contentType(MediaTypes.HAL_JSON_UTF8_VALUE))
+        .andExpect(jsonPath("$._embedded.learningOutcomes[0].competence.action",
+            is(learningOutcomes.get(0).getCompetence().getAction())))
+        .andExpect(jsonPath("$._embedded.learningOutcomes[0].competence.taxonomyLevel",
+            is(learningOutcomes.get(0).getCompetence().getTaxonomyLevel().name())))
+        .andExpect(jsonPath("$._embedded.learningOutcomes[0].tools[0].value",
+            is(learningOutcomes.get(0).getTools().get(0).getValue())))
+        .andExpect(jsonPath("$._embedded.learningOutcomes[0].tools[1].value",
+            is(learningOutcomes.get(0).getTools().get(1).getValue())))
+        .andExpect(jsonPath("$._embedded.learningOutcomes[0].purpose.value",
+            is(learningOutcomes.get(0).getPurpose().getValue())))
+        .andExpect(jsonPath("$._embedded.learningOutcomes[0]._links.self", notNullValue()))
+        .andExpect(jsonPath("$._embedded.learningOutcomes[1].competence.action",
+            is(learningOutcomes.get(1).getCompetence().getAction())))
+        .andExpect(jsonPath("$._embedded.learningOutcomes[1].competence.taxonomyLevel",
+            is(learningOutcomes.get(1).getCompetence().getTaxonomyLevel().name())))
+        .andExpect(jsonPath("$._embedded.learningOutcomes[1].tools[0].value",
+            is(learningOutcomes.get(1).getTools().get(0).getValue())))
+        .andExpect(jsonPath("$._embedded.learningOutcomes[1].tools[1].value",
+            is(learningOutcomes.get(1).getTools().get(1).getValue())))
+        .andExpect(jsonPath("$._embedded.learningOutcomes[1].purpose.value",
+            is(learningOutcomes.get(1).getPurpose().getValue())))
+        .andExpect(jsonPath("$._embedded.learningOutcomes[1]._links.self", notNullValue()));
 
   }
 
@@ -133,6 +258,5 @@ public class CoalbaseLearningOutcomeApplicationTest {
         Arrays.asList(tool0, tool1), purpose);
 
   }
-
 
 }
