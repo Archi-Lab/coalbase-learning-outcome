abd3da306f76d20329e8d0ff1bd820265cfa29e3
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-02-07 13:59:19 +0100

    update inti.sql

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,3 +1,3 @@
-create table learning_outcome (uuid binary not null, action varchar(255), taxonomy_level integer, value varchar(255), primary key (uuid));
-create table learning_outcome_tools (learning_outcome_uuid binary not null, value varchar(255));
+create table learning_outcome (uuid bytea not null, action varchar(255), taxonomy_level integer, value varchar(255), primary key (uuid));
+create table learning_outcome_tools (learning_outcome_uuid bytea not null, value varchar(255));
 alter table learning_outcome_tools add constraint FKn67wjwkrqsphxl3lg6bi0adc7 foreign key (learning_outcome_uuid) references learning_outcome;
