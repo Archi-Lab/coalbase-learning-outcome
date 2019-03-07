3fb6c5d82789d20a5526c788d86247a24b278038
Marc Schmidt <marc.schmidt1@smail.th-koeln.de>
2019-03-07 13:45:09 +0100

    add tests for put, patch, delete of learningSpaces

--- src/test/java/de/archilab/coalbase/learningoutcomeservice/learningspace/AggregateLearningSpaceTest.java
+++ src/test/java/de/archilab/coalbase/learningoutcomeservice/learningspace/AggregateLearningSpaceTest.java
@@ -7,7 +7,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_CLASS;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,6 +27,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
@@ -46,6 +52,7 @@ import java.util.concurrent.TimeUnit;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import de.archilab.coalbase.learningoutcomeservice.core.UniqueId;
 import de.archilab.coalbase.learningoutcomeservice.learningoutcome.Competence;
 import de.archilab.coalbase.learningoutcomeservice.learningoutcome.LearningOutcome;
 import de.archilab.coalbase.learningoutcomeservice.learningoutcome.LearningOutcomeRepository;
@@ -163,6 +170,294 @@ public class AggregateLearningSpaceTest {
         .isEqualTo(LearningSpaceEventType.CREATED.name());
     assertThat(learningSpaceDomainEvent.getLearningSpaceIdentifier())
         .isEqualTo(learningSpace.getId());
+  }
+
+  @Test
+  public void canNotPostLearningSpaceWithoutAuthentication() throws Exception {
+    LearningSpace learningSpaceToPost = this.buildSampleLearningSpaceWithRequirment();
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    String learningSpaceAsJsonString = objectMapper.writeValueAsString(learningSpaceToPost);
+    JSONObject jsonObject = new JSONObject(learningSpaceAsJsonString);
+    jsonObject.put("learningOutcome",
+        "learningOutcomes/" + learningSpaceToPost.getLearningOutcome()
+            .getId());
+    jsonObject.put("requirement",
+        "learningSpaces/" + learningSpaceToPost.getRequirement()
+            .getId());
+    String learningSpaceAsJson = jsonObject.toString();
+
+    this.mvc.perform(post("/learningSpaces").with(csrf()).content(learningSpaceAsJson)
+        .contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(401));
+  }
+
+  @Test
+  @WithMockUser(username = "testProfessor", roles = {"coalbase_professor"})
+  public void putLearningSpace() throws Exception {
+    LearningSpace learningSpaceToPut = this.buildSampleLearningSpaceWithRequirment();
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    String learningSpaceAsJsonString = objectMapper.writeValueAsString(learningSpaceToPut);
+    JSONObject jsonObject = new JSONObject(learningSpaceAsJsonString);
+    jsonObject.put("learningOutcome",
+        "learningOutcomes/" + learningSpaceToPut.getLearningOutcome()
+            .getId());
+    jsonObject.put("requirement",
+        "learningSpaces/" + learningSpaceToPut.getRequirement()
+            .getId());
+    String learningSpaceAsJson = jsonObject.toString();
+
+    this.mvc.perform(put("/learningSpaces/" + learningSpaceToPut.getId().toString())
+        .content(learningSpaceAsJson).with(csrf())
+        .contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(201))
+        .andExpect(
+            jsonPath("$.title", is(learningSpaceToPut.getTitle())))
+        .andExpect(
+            jsonPath("$._links.learningOutcome", notNullValue()))
+        .andExpect(
+            jsonPath("$._links.requirement", notNullValue()))
+        .andExpect(
+            jsonPath("$._links.self", notNullValue())
+        );
+    List<LearningSpace> learningSpaceList = (List<LearningSpace>) this.learningSpaceRepository
+        .findAll();
+    assertEquals(learningSpaceList.size(), 2);
+    Optional<LearningSpace> optionalLearningSpace = this.learningSpaceRepository
+        .findById(learningSpaceList.get(1).getId());
+    assertTrue(optionalLearningSpace.isPresent());
+    LearningSpace learningSpace = optionalLearningSpace.get();
+    assertEquals(learningSpace.getTitle(), learningSpaceToPut.getTitle());
+    assertEquals(learningSpace.getLearningOutcome(), learningSpaceToPut.getLearningOutcome());
+    assertEquals(learningSpace.getRequirement(), learningSpaceToPut.getRequirement());
+
+    /*Test kafka message */
+    ConsumerRecord<String, String> record = AggregateLearningSpaceTest.records
+        .poll(10, TimeUnit.SECONDS);
+    LearningSpaceDomainEvent learningSpaceDomainEvent = objectMapper
+        .readValue(record.value(), LearningSpaceDomainEvent.class);
+    assertThat(learningSpaceDomainEvent.getEventType())
+        .isEqualTo(LearningSpaceEventType.CREATED.name());
+    assertThat(learningSpaceDomainEvent.getLearningSpaceIdentifier())
+        .isEqualTo(learningSpace.getId());
+  }
+
+  @Test
+  @WithMockUser(username = "testProfessor", roles = {"coalbase_professor"})
+  public void expectUpdateEventOnSecondPutLearningSpace() throws Exception {
+    LearningSpace learningSpaceToPut = this.buildSampleLearningSpaceWithRequirment();
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    String learningSpaceAsJsonString = objectMapper.writeValueAsString(learningSpaceToPut);
+    JSONObject jsonObject = new JSONObject(learningSpaceAsJsonString);
+    jsonObject.put("learningOutcome",
+        "learningOutcomes/" + learningSpaceToPut.getLearningOutcome()
+            .getId());
+    jsonObject.put("requirement",
+        "learningSpaces/" + learningSpaceToPut.getRequirement()
+            .getId());
+    String learningSpaceAsJson = jsonObject.toString();
+
+    this.mvc.perform(put("/learningSpaces/" + learningSpaceToPut.getId().toString())
+        .content(learningSpaceAsJson).with(csrf())
+        .contentType(MediaType.APPLICATION_JSON));
+
+    /*Test kafka message */
+    ConsumerRecord<String, String> record = AggregateLearningSpaceTest.records
+        .poll(10, TimeUnit.SECONDS);
+    LearningSpaceDomainEvent learningSpaceDomainEvent = objectMapper
+        .readValue(record.value(), LearningSpaceDomainEvent.class);
+    assertThat(learningSpaceDomainEvent.getEventType())
+        .isEqualTo(LearningSpaceEventType.CREATED.name());
+
+    this.mvc.perform(put("/learningSpaces/" + learningSpaceToPut.getId().toString())
+        .content(learningSpaceAsJson).with(csrf())
+        .contentType(MediaType.APPLICATION_JSON));
+
+    List<LearningSpace> learningSpaceList = (List<LearningSpace>) this.learningSpaceRepository
+        .findAll();
+    assertEquals(learningSpaceList.size(), 2);
+    Optional<LearningSpace> optionalLearningSpace = this.learningSpaceRepository
+        .findById(learningSpaceList.get(1).getId());
+    assertTrue(optionalLearningSpace.isPresent());
+    LearningSpace learningSpace = optionalLearningSpace.get();
+    assertEquals(learningSpace.getTitle(), learningSpaceToPut.getTitle());
+    assertEquals(learningSpace.getLearningOutcome(), learningSpaceToPut.getLearningOutcome());
+    assertEquals(learningSpace.getRequirement(), learningSpaceToPut.getRequirement());
+
+    /*Test kafka message */
+    ConsumerRecord<String, String> secondRecord = AggregateLearningSpaceTest.records
+        .poll(10, TimeUnit.SECONDS);
+    LearningSpaceDomainEvent secondLearningSpaceDomainEvent = objectMapper
+        .readValue(secondRecord.value(), LearningSpaceDomainEvent.class);
+    assertThat(secondLearningSpaceDomainEvent.getEventType())
+        .isEqualTo(LearningSpaceEventType.UPDATED.name());
+    assertThat(secondLearningSpaceDomainEvent.getLearningSpaceIdentifier())
+        .isEqualTo(learningSpace.getId());
+  }
+
+  @Test
+  public void canNotPutLearningSpaceWithoutAuthentication() throws Exception {
+    LearningSpace learningSpaceToPut = this.buildSampleLearningSpaceWithRequirment();
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    String learningSpaceAsJsonString = objectMapper.writeValueAsString(learningSpaceToPut);
+    JSONObject jsonObject = new JSONObject(learningSpaceAsJsonString);
+    jsonObject.put("learningOutcome",
+        "learningOutcomes/" + learningSpaceToPut.getLearningOutcome()
+            .getId());
+    jsonObject.put("requirement",
+        "learningSpaces/" + learningSpaceToPut.getRequirement()
+            .getId());
+    String learningSpaceAsJson = jsonObject.toString();
+
+    this.mvc.perform(put("/learningSpaces/" + learningSpaceToPut.getId().toString())
+        .content(learningSpaceAsJson).with(csrf())
+        .contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(401));
+  }
+
+  @Test
+  @WithMockUser(username = "testProfessor", roles = {"coalbase_professor"})
+  public void patchLearningSpace() throws Exception {
+    UniqueId<LearningSpace> identifier = this.buildAndSaveLearningSpaceWithRequirment();
+    Optional<LearningSpace> optionalLearningSpace = this.learningSpaceRepository
+        .findById(identifier);
+
+    LearningSpace learningSpace = optionalLearningSpace.orElse(null);
+    assertThat(learningSpace).isNotNull();
+
+    learningSpace.setTitle("NewTitleAfterPatch");
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    String learningSpaceAsJsonString = objectMapper.writeValueAsString(learningSpace);
+    JSONObject jsonObject = new JSONObject(learningSpaceAsJsonString);
+    jsonObject.put("learningOutcome",
+        "learningOutcomes/" + learningSpace.getLearningOutcome()
+            .getId());
+    jsonObject.put("requirement",
+        "learningSpaces/" + learningSpace.getRequirement()
+            .getId());
+    String learningSpaceAsJson = jsonObject.toString();
+
+    this.mvc.perform(patch("/learningSpaces/" + learningSpace.getId().toString())
+        .content(learningSpaceAsJson).with(csrf())
+        .contentType(MediaType.APPLICATION_JSON)).andExpect(status().is(200))
+        .andExpect(
+            jsonPath("$.title", is(learningSpace.getTitle())))
+        .andExpect(
+            jsonPath("$._links.learningOutcome", notNullValue()))
+        .andExpect(
+            jsonPath("$._links.requirement", notNullValue()))
+        .andExpect(
+            jsonPath("$._links.self", notNullValue())
+        );
+
+    /*Test kafka message */
+    ConsumerRecord<String, String> record = AggregateLearningSpaceTest.records
+        .poll(10, TimeUnit.SECONDS);
+    LearningSpaceDomainEvent learningSpaceDomainEvent = objectMapper
+        .readValue(record.value(), LearningSpaceDomainEvent.class);
+    assertThat(learningSpaceDomainEvent.getEventType())
+        .isEqualTo(LearningSpaceEventType.UPDATED.name());
+    assertThat(learningSpaceDomainEvent.getLearningSpaceIdentifier())
+        .isEqualTo(learningSpace.getId());
+  }
+
+  @Test
+  @WithMockUser(username = "testProfessor", roles = {"coalbase_professor"})
+  public void deleteLearningSpace() throws Exception {
+    UniqueId<LearningSpace> identifier = this.buildAndSaveLearningSpaceWithRequirment();
+    Optional<LearningSpace> optionalLearningSpace = this.learningSpaceRepository
+        .findById(identifier);
+
+    LearningSpace learningSpace = optionalLearningSpace.orElse(null);
+    assertThat(learningSpace).isNotNull();
+
+    this.mvc.perform(delete("/learningSpaces/" + learningSpace.getId().toString()).with(csrf()))
+        .andExpect(status().isNoContent());
+
+    Optional<LearningSpace> optionalLearningSpaceDeleted = this.learningSpaceRepository
+        .findById(identifier);
+    assertThat(optionalLearningSpaceDeleted.isPresent()).isFalse();
+
+    ConsumerRecord<String, String> record = AggregateLearningSpaceTest.records
+        .poll(10, TimeUnit.SECONDS);
+    LearningSpaceDomainEvent learningSpaceDomainEvent = new ObjectMapper()
+        .readValue(record.value(), LearningSpaceDomainEvent.class);
+    assertThat(learningSpaceDomainEvent.getEventType())
+        .isEqualTo(LearningSpaceEventType.DELETED.name());
+    assertThat(learningSpaceDomainEvent.getLearningSpaceIdentifier())
+        .isEqualTo(learningSpace.getId());
+  }
+
+  @Test
+  public void canNotDeleteLearningSpaceWithoutAuthentication() throws Exception {
+    this.mvc.perform(delete("learningSpaces/1").with(csrf())).andExpect(status().is(401));
+  }
+
+  @Test
+  public void getLearningSpaceByID() throws Exception {
+    UniqueId<LearningSpace> identifier = this.buildAndSaveLearningSpaceWithRequirment();
+    Optional<LearningSpace> optionalLearningSpace = this.learningSpaceRepository
+        .findById(identifier);
+
+    LearningSpace learningSpace = optionalLearningSpace.orElse(null);
+    assertThat(learningSpace).isNotNull();
+
+    this.mvc.perform(get("/learningSpaces/" + learningSpace.getId().toString()).with(csrf()))
+        .andExpect(content().contentType(MediaTypes.HAL_JSON_UTF8_VALUE))
+        .andExpect(status().is(200))
+        .andExpect(
+            jsonPath("$.title", is(learningSpace.getTitle())))
+        .andExpect(
+            jsonPath("$._links.learningOutcome", notNullValue()))
+        .andExpect(
+            jsonPath("$._links.requirement", notNullValue()))
+        .andExpect(
+            jsonPath("$._links.self", notNullValue())
+        );
+  }
+
+  @Test
+  public void getLearningSpaces() throws Exception {
+    this.buildAndSaveLearningSpaceWithRequirment();
+
+    List<LearningSpace> learningSpaces = (List<LearningSpace>) this.learningSpaceRepository
+        .findAll();
+
+    this.mvc.perform(get("/learningSpaces").with(csrf()))
+        .andExpect(content().contentType(MediaTypes.HAL_JSON_UTF8_VALUE))
+        .andExpect(status().is(200))
+        .andExpect(
+            jsonPath("$._embedded.learningSpaces[0].title", is(learningSpaces.get(0).getTitle())))
+        .andExpect(
+            jsonPath("$._embedded.learningSpaces[0]._links.learningOutcome", notNullValue()))
+        .andExpect(
+            jsonPath("$._embedded.learningSpaces[0]._links.requirement", notNullValue()))
+        .andExpect(
+            jsonPath("$._embedded.learningSpaces[0]._links.self", notNullValue())
+        )
+        .andExpect(
+            jsonPath("$._embedded.learningSpaces[1].title", is(learningSpaces.get(1).getTitle())))
+        .andExpect(
+            jsonPath("$._embedded.learningSpaces[1]._links.learningOutcome", notNullValue()))
+        .andExpect(
+            jsonPath("$._embedded.learningSpaces[1]._links.requirement", notNullValue()))
+        .andExpect(
+            jsonPath("$._embedded.learningSpaces[1]._links.self", notNullValue())
+        );
+
+  }
+
+
+  private UniqueId<LearningSpace> buildAndSaveLearningSpaceWithRequirment() {
+    final LearningSpace learningSpace = this.buildSampleLearningSpaceWithRequirment();
+    this.learningSpaceRepository.save(learningSpace);
+    return learningSpace.getId();
   }
 
   private LearningSpace buildSampleLearningSpaceWithRequirment() {
