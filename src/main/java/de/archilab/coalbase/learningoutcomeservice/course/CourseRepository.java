82bbe80331ef1bbfd86503f0d27728bd369d1bcb
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-04-16 21:52:06 +0200

    remove unused cors on repos

--- src/main/java/de/archilab/coalbase/learningoutcomeservice/course/CourseRepository.java
+++ src/main/java/de/archilab/coalbase/learningoutcomeservice/course/CourseRepository.java
@@ -3,12 +3,10 @@ package de.archilab.coalbase.learningoutcomeservice.course;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PostFilter;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import de.archilab.coalbase.learningoutcomeservice.core.UniqueId;
 
 @RepositoryRestResource
-@CrossOrigin("*")
 public interface CourseRepository extends
     CrudRepository<Course, UniqueId<Course>> {
 
--- src/main/java/de/archilab/coalbase/learningoutcomeservice/learningoutcome/LearningOutcomeRepository.java
+++ src/main/java/de/archilab/coalbase/learningoutcomeservice/learningoutcome/LearningOutcomeRepository.java
@@ -2,12 +2,10 @@ package de.archilab.coalbase.learningoutcomeservice.learningoutcome;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import de.archilab.coalbase.learningoutcomeservice.core.UniqueId;
 
 @RepositoryRestResource
-@CrossOrigin("*")
 public interface LearningOutcomeRepository extends
     CrudRepository<LearningOutcome, UniqueId<LearningOutcome>> {
 
